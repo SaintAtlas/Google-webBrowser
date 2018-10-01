923e3b13ac934bb13a304331a0450ed47aab49c3
Grant Farmer <Grant.Farmer21@gmail.com>
2018-10-01 15:10:46 -0400

    Update

--- webBrowser/ViewController.swift
+++ webBrowser/ViewController.swift
@@ -7,14 +7,39 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var myWebView: WKWebView!
+    
+    @IBAction func back(_ sender: Any) {
+        if myWebView.canGoBack {
+            myWebView.goBack()
+        }
+    }
+    @IBAction func refresh(_ sender: Any)
+    {
+        myWebView.reload()
+    }
+    @IBAction func next(_ sender: Any) {
+        if myWebView.canGoForward {
+            myWebView.goForward()
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        myWebView.load(URLRequest(url: URL(string: "https://www.google.com")!))
     }
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // dispose of any resources that can be created
+    }
 
 }
 
